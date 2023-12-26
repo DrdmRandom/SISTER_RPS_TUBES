3660b1c2567252cb2ab66b3a032a92e2bca9096c
DrdmRandom <117142698+DrdmRandom@users.noreply.github.com>
2023-12-26 17:40:06 +0700

    improving the code

--- server/Server.py
+++ server/Server.py
@@ -1,57 +1,76 @@
 import socket
 import threading
 
-# Server setup
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow the socket to reuse the address
-server.bind(('192.168.1.14', 5001))  # Use a valid IP and port
-server.listen()  # Set the number of unaccepted connections that the system will allow before refusing new connections
-
-# Store client info
 clients = []
-choices = [None, None]
+choices = []
+clients_lock = threading.Lock()
 
-# Synchronization primitive
-choices_made = threading.Condition()
+def rps_game(c1_choice, c2_choice):
+    if c1_choice == c2_choice:
+        return "Draw"
+    elif (c1_choice == "Rock" and c2_choice == "Scissors") or \
+            (c1_choice == "Scissors" and c2_choice == "Paper") or \
+            (c1_choice == "Paper" and c2_choice == "Rock"):
+        return "Client 1 Wins"
+    else:
+        return "Client 2 Wins"
 
-def client_thread(connection, player):
+def handle_client(conn, addr):
     global choices
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    choice = conn.recv(1024).decode('utf-8')
+    with clients_lock:
+        choices.append((addr, choice))
+        print(f"[CHOICE] Client {addr} chose {choice}")
+
+    while len(choices) < 2:
+        continue
+
+    with clients_lock:
+        c1_addr, c1_choice = choices[0]
+        c2_addr, c2_choice = choices[1]
+        result = rps_game(c1_choice, c2_choice)
+        print(f"[RESULT] {result}")
+
+        for client in clients:
+            if client.fileno() != -1:
+                try:
+                    client.sendall(result.encode('utf-8'))
+                except Exception as e:
+                    print(f"Error sending data to {client}: {e}")
+
+    conn.close()
+    with clients_lock:
+        clients.remove(conn)
+
+def get_server_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        connection.sendall('Welcome to Rock Paper Scissors! Please choose Rock, Paper, or Scissor:'.encode())
-        choice = connection.recv(1024).decode().strip()
-        with choices_made:
-            choices[player] = choice
-            # Check if both players made a choice
-            choices_made.notify_all()  # Notify any waiting threads
-            choices_made.wait_for(lambda: all(c is not None for c in choices))  # Wait until all choices are made
-
-        result = determine_winner(choices[0], choices[1])
-        connection.sendall(result.encode())
+        s.connect(("8.8.8.8", 80))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = "127.0.0.1"
     finally:
-        connection.close()
+        s.close()
+    return IP
 
-def determine_winner(choice1, choice2):
-    def determine_winner(choice1, choice2):
-        # Mapping of what beats what
-        beats = {'Rock': 'Scissors', 'Scissors': 'Paper', 'Paper': 'Rock'}
+def start_server():
+    server_ip = get_server_ip()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((server_ip, 55559))
+    server.listen(2)
 
-        if choice1 == choice2:
-            return "It's a tie!"
-        elif beats[choice1] == choice2:
-            return "Player 1 wins!"
-        else:
-            return "Player 2 wins!"
-    pass
+    print(f"[SERVER IP] The server IP is: {server_ip}")
 
-# Function to start the game and accept clients
-def start_game():
-    while len(clients) < 2:  # We need exactly 2 clients for a game
+    while len(clients) < 2:
         conn, addr = server.accept()
-        print(f"Connected by {addr}")
-        clients.append(conn)
-        player_number = len(clients) - 1
-        thread = threading.Thread(target=client_thread, args=(conn, player_number))
+        with clients_lock:
+            clients.append(conn)
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
 
-# Start the game
-start_game()
+    print(f"[STARTING] Game starting with {len(clients)} players.")
+
+if __name__ == "__main__":
+    start_server()
